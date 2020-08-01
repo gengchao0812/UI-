0210fff1d48ec6ac41db76167c576aeea1b14ca3
gengchao0812 <gengchao0812@126.com>
2020-08-02 02:15:08 +0800

    UI 自动化框架 测试用例的封装

--- test_cast/test_main.py
+++ test_cast/test_main.py
@@ -1,14 +1,16 @@
 import pytest
 import yaml
+from test_cast.test_base import TestBase
+
 from page.app import App
 
 
-class TestMain:
+class TestMain(TestBase):
 
     #测试数据的驱动
+    #测试用例的封装
     @pytest.mark.parametrize("value1,value2",yaml.safe_load(open("./test_main.yaml")))
     def test_main(self,value1,value2):
-        app = App()
-        app.start().main().goto_search()
+        self.app.start().main().goto_search()
         # print(value1)
         # print(value2)
