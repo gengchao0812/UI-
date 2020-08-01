de89daeeda994bad6cba871423592bddcffaffea
gengchao0812 <gengchao0812@126.com>
2020-08-02 02:02:24 +0800

    UI 自动化框架 捕获异常

--- page/base_page.py
+++ page/base_page.py
@@ -1,14 +1,27 @@
 import yaml
 from appium.webdriver.webdriver import WebDriver
+from webdriver.common.by import By
+from webdriver.remote.mobile import Mobile
 
 
 class BasePage:
     _driver: WebDriver = None
+    _black_list=[(By.id,"iv_close")]
     def __init__(self,driver:WebDriver = None):
         self._driver = driver
 
     def find(self,locator,value):
-        return self._driver.find_element(locator,value)
+        try:
+            element = self._driver.find_element(locator, value)
+            return element
+        #捕获异常
+        except:
+            for black in self._black_list:
+                elements = self.find_elements(*black)
+                if len(elements) > 0:
+                    elements[0].click()
+                    break
+            return self.find(locator,value)
 
     #测试步骤的驱动
     def steps(self,path):
